de8b63659c4dba10847d604315209d42ac30869f
Colin Reinhardt <reinhardtc22@gmail.com>
2025-03-06 14:56:06 -0500

    displayed repos on page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,15 +1,54 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
 function getUserRepos(user) {
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
   fetch(apiUrl).then(function (response) {
     response.json().then(function (data) {
-      console.log(data);
+      displayRepos(data, user);
     });
   });
 }
 
-function userNameInput(callback) {
-  var userName = window.prompt("Enter your GitHub username");
-  callback(userName);
+function formSubmitHandler(event) {
+  event.preventDefault();
+  var username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+  } else {
+    alert("Please enter a GitHub username");
+  }
 }
 
-userNameInput(getUserRepos);
+var displayRepos = function (repos, searchTerm) {
+  console.log(repos);
+  console.log(searchTerm);
+  //clear old content
+  repoContainerEl.textContent = "";
+  repoSearchTerm.textContent = searchTerm;
+
+  for (let i = 0; i < repos.length; i++) {
+    //format repo name from data
+    var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+    //create container for each repo
+    var repoEl = document.createElement("div");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+    // create a sppan element to hold repo name
+    var titleEl = document.createElement("span");
+    titleEl.textContent = repoName;
+
+    //append to container
+    repoEl.appendChild(titleEl);
+
+    //append container to DOM
+    repoContainerEl.appendChild(repoEl);
+  }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
